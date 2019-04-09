13810589c93d8c9c9483963395242b767923008e
James Ault <james.ault@johnlewis.co.uk>
2019-04-09 12:38:41 +0100

    Passing test for print bank statement

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -23,7 +23,7 @@ it('should be able to deposit', function(){
 
 it('should be able to deposit with a date', function(){
   var depositAmount = 1000
-  var depositDate = new Date('10-01-2012')
+  var depositDate = '10/01/2012'
   bank.deposit(depositAmount,depositDate);
   expect(bank.statement[0].date).toEqual(depositDate)
 })
@@ -36,12 +36,42 @@ it('should be able to withdraw', function(){
 
 it('should be able to withdraw with a date', function(){
   var withdrawlAmount = 500
-  var withdrawlDate = new Date('14-01-2012')
+  var withdrawlDate = '14/01/2012'
   bank.withdraw(withdrawlAmount,withdrawlDate);
   expect(bank.statement[0].date).toEqual(withdrawlDate)
 })
 
+it('should be able to display a statement in the console', function(){
+  var depositAmount = 1000
+  var depositDate = '10/01/2012'
+  expectedOutput = 'date || credit || debit || balance'
+  expectedOutput2 = '10/01/2012 || 1000 || null || 1000'
+  bank.deposit(depositAmount,depositDate);
+  bank.printStatement();
+  expect(console.log).toHaveBeenCalledWith(expectedOutput);
+  expect(console.log).toHaveBeenCalledWith(expectedOutput2);
+})
 
+it('should pass a feature test of the requirements',function(){
+  var depositAmount = 1000
+  var depositDate = '10/01/2012'
+  var depositAmount2 = 2000
+  var depositDate2 = '13/01/2012'
+  var withdrawlAmount = 500
+  var withdrawlDate = '14/01/2012'
+  bank.deposit(depositAmount,depositDate);
+  bank.deposit(depositAmount2,depositDate2);
+  bank.withdraw(withdrawlAmount,withdrawlDate);
+  expectedOutput = 'date || credit || debit || balance'
+  expectedOutput2 = '14/01/2012 || null || 500 || 2500'
+  expectedOutput3 = '13/01/2012 || 2000 || null || 3000'
+  expectedOutput4 = '10/01/2012 || 1000 || null || 1000'
+  bank.printStatement();
+  expect(console.log).toHaveBeenCalledWith(expectedOutput);
+  expect(console.log).toHaveBeenCalledWith(expectedOutput2);
+  expect(console.log).toHaveBeenCalledWith(expectedOutput3);
+  expect(console.log).toHaveBeenCalledWith(expectedOutput4);
+})
 
 
 
--- src/Bank.js
+++ src/Bank.js
@@ -5,12 +5,19 @@ function Bank(){
 
 Bank.prototype.deposit = function (depositAmount = null, depositDate = new Date()) {
   this.balance += depositAmount
-  this.statement.push({date: depositDate, credit: depositAmount, debit: null, balance: this.balance})
+  this.statement.unshift({date: depositDate, credit: depositAmount, debit: null, balance: this.balance})
 }
 
 Bank.prototype.withdraw = function (withdrawlAmount = null, withdrawlDate = new Date()) {
   this.balance -= withdrawlAmount;
-  this.statement.push({date: withdrawlDate, credit: null, debit: withdrawlAmount, balance: this.balance})
+  this.statement.unshift({date: withdrawlDate, credit: null, debit: withdrawlAmount, balance: this.balance})
 } 
+
+Bank.prototype.printStatement = function(){
+  console.log('date || credit || debit || balance')
+  this.statement.forEach(function(entry){
+    console.log(entry['date'] + ' || ' + entry['credit'] + ' || ' + entry['debit'] + ' || ' + entry['balance'])
+  })
+}
 
 module.exports = Bank;
